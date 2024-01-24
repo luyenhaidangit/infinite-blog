7dcdcdd44d608639e41ef4824980d05e57b3828b
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-01-25 00:56:32 +0700

    Init structure project

---
+++ src/InfiniteBlog.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfiniteBlog.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
