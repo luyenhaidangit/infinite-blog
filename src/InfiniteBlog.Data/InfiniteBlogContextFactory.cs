b4a2fb3938b380b3751a811e3df87cc79cc07e79
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-01-27 09:40:14 +0700

    Init migration project

---
+++ src/InfiniteBlog.Data/InfiniteBlogContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteBlog.Data
+{
+    public class InfiniteBlogContextFactory : IDesignTimeDbContextFactory<InfiniteBlogContext>
+    {
+        public InfiniteBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<InfiniteBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new InfiniteBlogContext(builder.Options);
+        }
+    }
+}
