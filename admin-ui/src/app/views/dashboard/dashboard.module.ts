fc1c3166efe9f60087d029cbd1a6ebeae596926e
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-02-07 02:51:10 +0700

    Implement menu and design dashboard

--- admin-ui/src/app/views/dashboard/dashboard.module.ts
+++ admin-ui/src/app/views/dashboard/dashboard.module.ts
@@ -1,11 +1,46 @@
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
+import {
+  AvatarModule,
+  ButtonGroupModule,
+  ButtonModule,
+  CardModule,
+  FormModule,
+  GridModule,
+  NavModule,
+  ProgressModule,
+  TableModule,
+  TabsModule
+} from '@coreui/angular';
+
+import { WidgetsModule } from '../widgets/widgets.module';
+import { ChartjsModule } from '@coreui/angular-chartjs';
+import { IconModule } from '@coreui/icons-angular';
+
 @NgModule({
   imports: [
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    WidgetsModule,
+    CardModule,
+    GridModule,
+    NavModule,
+    ButtonGroupModule,
+    FormModule,
+    ReactiveFormsModule,
+    ProgressModule,
+    ChartjsModule,
+    CommonModule,
+    TableModule,
+    AvatarModule,
+    ButtonModule,
+    TabsModule,
+    IconModule
   ],
   declarations: [DashboardComponent]
 })
